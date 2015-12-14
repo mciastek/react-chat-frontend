f8f6a53a34e3eecb3f6fd0e90afcc2f5a7b31f3d
Mirosław Ciastek <miroslaw.ciastek@elpassion.com>
2015-12-14 20:42:45 +0100

    Added redirect when user name is present in session storage.

--- src/components/run.js
+++ src/components/run.js
@@ -9,12 +9,19 @@ import Login from './Login';
 import Dashboard from './Dashboard';
 
 const history = createBrowserHistory();
+const sessionStorage = window.sessionStorage;
+
+var checkIfAuth = (nextState, transition) => {
+  if (sessionStorage.getItem('userName')) {
+    transition(null, 'chat');
+  }
+};
 
 // Render the main component into the dom
 ReactDOM.render(
   <Router history={history}>
     <Route path="/" component={App}>
-      <IndexRoute component={Login} />
+      <IndexRoute component={Login} onEnter={checkIfAuth} />
       <Route path="chat" component={Dashboard} />
     </Route>
   </Router>
