d5ea719d75256b157f7261f12c63cf3b1c182a26
Mirosław Ciastek <miroslaw.ciastek@elpassion.com>
2015-12-14 20:42:16 +0100

    Added saving user name to session storage and redirect after submitting it.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,26 @@
 require('styles/Login.scss');
 
 import React from 'react';
+import { PropTypes } from 'react-router'
+
+const sessionStorage = window.sessionStorage;
 
 class LoginComponent extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { userName: '' };
+  }
+
+  handleTextChange(event) {
+    this.setState({ userName: event.target.value });
+  }
+
+  handleSubmit() {
+    sessionStorage.setItem('userName', this.state.userName);
+    this.context.history.pushState(null, 'chat');
+  }
+
   render() {
     return (
       <section className="login-form jumbotron">
@@ -11,15 +29,19 @@ class LoginComponent extends React.Component {
           <div className="form-group">
             <div className="input-group">
               <span className="input-group-addon">@</span>
-              <input type="text" className="form-control" placeholder="Username" />
+              <input type="text" className="form-control" placeholder="Username" value={this.state.userName} onChange={this.handleTextChange.bind(this)} />
             </div>
           </div>
           <div className="form-group">
-            <button type="button" className="btn btn-primary-outline">Login</button>
+            <button type="button" className="btn btn-primary-outline" onClick={this.handleSubmit.bind(this)}>Login</button>
           </div>
       </section>
     );
   }
 }
+
+LoginComponent.contextTypes = {
+  history: PropTypes.history
+};
 
 export default LoginComponent;
